6835b40560bd49105dce5c3ba0695a8738b62163
Ahmadusaidh M <149071116+usaidhahamed1011@users.noreply.github.com>
2025-02-27 09:34:20 +0530

    Delete AirlineReservationSystem directory

--- AirlineReservationSystem/src/main/java/com/example/AirlineReservationSystem/Repo/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AirlineReservationSystem.Repo;
-
-import com.example.AirlineReservationSystem.Entity.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-}
